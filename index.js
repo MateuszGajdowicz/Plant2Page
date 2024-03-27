d87afebec4baea4c104544e5916d7b9f46d8cf1b
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2024-03-27 13:44:26 +0100

    wq

--- index.js
+++ index.js
@@ -5,17 +5,17 @@
 //    let WateringDate = document.getElementById("p1");
 //    WateringDate.textContent = Date.toLocalString();
 //    }
-function waterPlant() {
+function waterPlant2() {
         var currentDate = new Date();
-        var formattedDate = currentDate.toLocaleString();
-        var WateringDate = document.getElementById('p1');
-        WateringDate.textContent = formattedDate;
+        var formattedDate2 = currentDate.toLocaleString();
+        var WateringDate2 = document.getElementById('p1');
+        WateringDate2.textContent = formattedDate2;
 
         window.location.href = 'https://mateuszgajdowicz.github.io/CongratulationsPage/';
-        localStorage.setItem("lastWateringDate", formattedDate);
+        localStorage.setItem("lastWateringDate2", formattedDate2);
         }
 window.onload = function(){
-    var lastWateringDate = localStorage.getItem("lastWateringDate");
-    if(lastWateringDate){
-    document.getElementById('p1').textContent =  lastWateringDate;}}
+    var lastWateringDate2 = localStorage.getItem("lastWateringDate2");
+    if(lastWateringDate2){
+    document.getElementById('p1').textContent =  lastWateringDate2;}}
 
